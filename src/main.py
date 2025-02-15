81992d0d73204aaa4b7f6a8437697473b6bf8c72
beeX <beeXgit@gmail.com>
2025-02-15 13:05:57 +0100

    set up text types and nodes; added tests; need to work on htmlnode next

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+def main():
+
+    testNode = TextNode('blab bla blabla blab lbabl', TextType.bold, 'https://www.boot.dev')
+    
+    print(testNode)
+
+    return
+
+main()
