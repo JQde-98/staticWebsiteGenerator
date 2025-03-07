aa90f81406d4b0971f62dcf660cae6b92850487a
beeX <beeXgit@gmail.com>
2025-03-07 15:42:14 +0100

    start of the main function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-from textnode import TextNode, TextType
+import os 
+import shutil
+
+def copy_static(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    
+    os.mkdir(dest_dir)
+    items = os.listdir(source_dir)
+
+    for item in items:
+        source_path = os.path.join(source_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+            print(f"Copied file: {source_path} to {dest_path}")
+        else:
+            os.mkdir(dest_path)
+            copy_static(source_path, dest_path)
 
 def main():
+    copy_static("static", "public")
 
-    testNode = TextNode('blab bla blabla blab lbabl', TextType.bold, 'https://www.boot.dev')
-    
-    print(testNode)
-
-    return
-
-main()
+if __name__ == "__main__":
+    main()
